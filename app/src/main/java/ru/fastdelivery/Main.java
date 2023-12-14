93afab95547e12b82a08c61452587c2e897c148c
sergei_darin <swd86@mail.ru>
2023-12-14 23:43:03 +0400

    Изменения комментариях

--- app/src/main/java/ru/fastdelivery/Main.java
+++ app/src/main/java/ru/fastdelivery/Main.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 /**
  * Класс запускающий приложение
+ *
  */
 @SpringBootApplication(scanBasePackages = { "ru.fastdelivery" })
 @ConfigurationPropertiesScan(value = { "ru.fastdelivery.properties" })
